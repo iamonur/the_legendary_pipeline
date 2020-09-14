dc8037164711efed29427228186f03e384d2040e
iamonur <otekik@icloud.com>
2020-09-14 11:42:46 +0300

    Two different plots, with log scaling.

--- src/goal_script/result_plot.py
+++ src/goal_script/result_plot.py
@@ -26,6 +26,28 @@ def get_file_list():
             csv_files.append([directory + "/" + file, int(wall), int(floor), int(portal)])
     return csv_files
 
+def get_file_list_2():
+    files_here = os.listdir()
+    for file in files_here:
+        if ".py" not in file:
+            directory = file
+            break
+    directory="zzz"
+    
+    os.chdir(directory)
+    files_here = os.listdir()
+    csv_files = []
+    for file in files_here:
+        if ".csv" in file:
+            temp = file.split('w')[1]
+            wall = temp.split('f')[0]
+            temp = temp.split('f')[1]
+            floor = temp.split('p')[0]
+            temp = temp.split('p')[1]
+            portal = temp.split('.')[0]
+            csv_files.append([directory + "/" + file, int(wall), int(floor), int(portal)])
+    return csv_files
+
 def parse_csv_file(file_structure):
     file = open(file_structure[0].split("/")[1], "r")
     lines = file.readlines()
@@ -90,6 +112,23 @@ def compute_on_all():
         all_results.append(temp_dict)
     return all_results
 
+def compute_on_all_2():
+    file_list = get_file_list_2()
+    all_results = []
+    for item in file_list:
+        result,best,worst = compute_on_results(parse_csv_file(item));
+        temp_dict = {
+            "wall":item[1],
+            "floor":item[2],
+            "portal":item[3],
+            "time":result[0],
+            "reward":result[1],
+            "win_rating":result[2],
+            "fitness":fitness(result[2], result[1], ( 9 * item[2] + item[3] ) )
+        }
+        all_results.append(temp_dict)
+    return all_results
+
 def group_on_portal_reward(all_results):
     ret_dict = {1:[],5:[],25:[],125:[],625:[],3125:[],15625:[],78125:[],390625:[]}
     for result in all_results:
@@ -130,9 +169,12 @@ def save_np_array_heatmap(arr, name):
 def save_alternate_graph(whole_data):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    portal_rewards=[625,15625,390625]
+    """portal_rewards=[625,15625,390625]
     wall_rewards=[-2048,-128,-32,-8,-2,0]
-    floor_rewards=[-2048,-128,-32,-8,-2,0]
+    floor_rewards=[-2048,-128,-32,-8,-2,0]"""
+    portal_rewards=[2,3,4]
+    wall_rewards=[11,7,5,3,1,-1]
+    floor_rewards=[11,7,5,3,1,-1]
     dp_count = len(whole_data)*len(whole_data[0])*len(whole_data[0][0])
     
     portal_reward = np.random.standard_normal(dp_count)
@@ -154,16 +196,19 @@ def save_alternate_graph(whole_data):
     ax.set_xlabel("Portal Reward")
     ax.set_ylabel("Wall Reward")
     ax.set_zlabel("Floor Reward")
-    plt.title("Graph")
+    plt.title("Graph #2")
     fig.colorbar(img)
     plt.show()
 
 def save_another_type_graph(whole_data):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    portal_rewards=[1,25,625,15625,390625]
-    wall_rewards=[-4096,-1024,-256,-64,-16,-4,-1]
-    floor_rewards=[-4096,-1024,-256,-64,-16,-4,-1]
+#    portal_rewards=[1,25,625,15625,390625]
+    portal_rewards=[0,1,2,3,4]#log25(x)
+#    wall_rewards=[4096,1024,256,64,16,4,1]
+    wall_rewards=[12,10,8,6,4,2,0]#log2(-x)
+#    floor_rewards=[4096,1024,256,64,16,4,1]
+    floor_rewards=[12,10,8,6,4,2,0]#log2(-x)
     dp_count = len(whole_data)*len(whole_data[0])*len(whole_data[0][0])
     
     portal_reward = np.random.standard_normal(dp_count)
@@ -185,7 +230,10 @@ def save_another_type_graph(whole_data):
     ax.set_xlabel("Portal Reward")
     ax.set_ylabel("Wall Reward")
     ax.set_zlabel("Floor Reward")
-    plt.title("Graph")
+    #ax.set_xscale('log')
+    #ax.set_yscale('log')
+    #ax.set_zscale('log')
+    plt.title("Graph #1")
     fig.colorbar(img)
     plt.show()
 
@@ -199,7 +247,7 @@ def main_1():
         save_np_array_heatmap(get_numpy_array_from_list(portal_dict[rew]),"Portal_reward:"+str(rew))
         plt.clf()
 
-def get_listoflist_alt(lili, to_be_plotted="win_rating"):
+def get_listoflist_2(lili, to_be_plotted="win_rating"):
         #Group them around wall first
 
     dictionary = {0:[],-2:[],-8:[],-32:[],-128:[],-2048:[]}
@@ -249,18 +297,24 @@ def get_listoflist(lili, to_be_plotted="win_rating"):
     return ret
 
 
-if __name__ == "__main__":
+def main_4D():
     #save_another_type_graph(None)
     portal_dict = group_on_portal_reward(compute_on_all())
     portal_rewards = [1,25,625,15625,390625]
-    alternate_pr = [625,15625,390625]
     lilili = []
-    lilili_alt = []
     for a in portal_rewards:
         lilili.append(get_listoflist(portal_dict[a]))
 
-    #for a in alternate_pr:
-    #    lilili_alt.append(get_listoflist_alt(portal_dict[a]))
-
     save_another_type_graph(lilili)
-    #save_alternate_graph(lilili_alt)
+
+def main_4D_2():
+    portal_dict = group_on_portal_reward(compute_on_all_2())
+    portal_rewards = [625,15625,390625]
+    lilili = []
+    for a in portal_rewards:
+        lilili.append(get_listoflist_2(portal_dict[a]))
+
+    save_alternate_graph(lilili)
+
+if __name__ == "__main__":
+    main_4D_2()
