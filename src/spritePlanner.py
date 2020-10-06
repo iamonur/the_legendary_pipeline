f4a0f7b5618c67fa68fb2096f83a4537d925e9ad
iamonur <otekik@icloud.com>
2020-10-06 07:54:12 +0300

    A new planner for one other game

--- src/spritePlanner.py
+++ src/spritePlanner.py
@@ -6,6 +6,12 @@ class spritePlannerException(Exception):
     pass
 
 class dualSpritePlanner:
+    """
+    Plans as:
+    - Portal at one corner
+    - Avatar at the opposite corner
+    #Best used in the game "MazeSolver"
+    """
     def __init__(self, map):
         self.map = map
         self.avatarAt = (-1,-1)
@@ -55,6 +61,13 @@ class dualSpritePlanner:
         return self.map
 
 class spritePlanner:
+    """
+    Plans as:
+    - Portal to one corner
+    - Avatar at the other corner
+    - Enemy in the middle
+    #This planner best be used in the game "Run". Because the avatar cannot race, and certainly not catch.
+    """
     def __init__(self, map, gameType='Basic'):
         self.gameType = gameType
         self.map = map
@@ -133,6 +146,13 @@ class spritePlanner:
         return self.map
 
 class reverseSpritePlanner:
+    """
+    Plans as:
+    - Portal at one corner
+    - Enemy at the other
+    - Avatar in the middle
+    #This planner best be used on the game "Catch". Because it can run and race so easily.
+    """
     def __init__(self, map, gameType='Basic'):
         self.gameType = gameType
         self.map = map
@@ -209,6 +229,92 @@ class reverseSpritePlanner:
             self.map[ln] = "".join(line)
 
         return self.map
+
+class equalSpritePlanner:
+    """
+    Plans as:
+    - Avatar at a corner.
+    - Enemy at the other.
+    - Portal in the middle.
+    # This planner better used on the game "race", since it minimizes conflict between the opponent and the avatar. However, remember that against the perfect opponent, this game has nearly 50% win chance.
+    """
+    def __init__(self, map, gameType='Basic'):
+        self.gameType = gameType
+        self.map = map
+        self.avatarAt = (-1,-1)
+        self.portalAt = (-1,-1)
+        self.enemyAt  = (-1,-1)
+
+    def findMostDistantCells(self):
+
+        maxDist = 0
+        cells =((-1,-1),(-1,-1))
+
+        for ln1, l1 in enumerate(self.map):
+            for cn1, c1 in enumerate(l1):
+                if c1 == "1":
+                    continue
+                for ln2, l2 in enumerate(self.map):
+                    for cn2, c2 in enumerate(l2):
+                        if c2 == '1':
+                            continue
+                        if (abs(ln1-ln2)+abs(cn1-cn2)) > maxDist:
+                            maxDist = (abs(ln1-ln2)+abs(cn1-cn2))
+                            cells = ((ln1,cn1),(ln2,cn2))
+
+        return cells
+
+    def findMidCell(self):
+        (y1, x1) = self.avatarAt
+        (y2, x2) = self.enemyAt
+        (yy, xx) = ((y1+y2)/2,(x1+x2)/2)
+        toRet = (-1, -1)
+        minDist = 2147483647
+
+        for ln, l in enumerate(self.map):
+            for cn, c in enumerate(l):
+                if c != '0':
+                    continue
+                if (ln,cn) == (y1,x1) or (ln,cn) == (y1,x1):
+                    continue
+                if (abs(yy - ln) + abs(xx - cn)) < minDist:
+                    minDist = abs(yy - ln) + abs(xx - cn)
+                    toRet = (ln, cn)
+                    if minDist == 0 or minDist == 0.5:
+                        return toRet
+
+        return toRet
+
+    def perform(self):
+        if self.gameType == 'Basic':
+            (self.avatarAt,self.enemyAt) = self.findMostDistantCells()
+            self.portalAt = self.findMidCell()
+            
+
+    def getMap(self):
+        if self.avatarAt == (-1,-1) or self.portalAt == (-1,-1) or self.enemyAt == (-1,-1):
+            raise spritePlannerException("Plan the sprites first before placing!")
+
+        dummy_map = []
+        for line in self.map:
+            dummy_map.append(list(line))
+
+        (y, x) = self.enemyAt
+        dummy_map[y][x] = 'A'
+
+        (y, x) = self.avatarAt
+        dummy_map[y][x] = 'E'
+
+        (y, x) = self.portalAt
+        dummy_map[y][x] = 'G'
+
+        
+
+        for ln, line in enumerate(dummy_map):
+            self.map[ln] = "".join(line)
+
+        return self.map
+
 
 if __name__ == "__main__":
     s = spritePlanner(caPolisher.polisher().perform())
