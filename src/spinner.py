ca7d4c9478ae28f9cde58ebcfa3a8ec148d4070f
iamonur <otekik@icloud.com>
2021-07-05 10:14:55 +0300

    New LTL: straights & turns

--- src/spinner.py
+++ src/spinner.py
@@ -940,6 +940,23 @@ typedef row{{ //This is for creating 2-D arrays, since they are not supported.
   int next_x;
   int next_y;
 """
+promela_header_for_game_3_smart_ltl_movement = """
+typedef row{{ //This is for creating 2-D arrays, since they are not supported.
+    byte a[{0}];
+  }}
+  c_code{{\#include "../spin/bfs.c"}};
+  bit win  = 0;
+  bit dead = 0;
+  bit turn = 0;
+  bit lock = 0;
+  row map[{1}]; //This is your 2-D array.
+  int last_move = -1;
+  int straights = 0;
+  int turns = 0;
+  int next_x;
+  int next_y;
+"""
+
 promela_avatar_sokoban = """
 proctype avatar_sokoban(int x; int y){
   map[x].a[y] = 2;
@@ -1358,6 +1375,101 @@ proctype avatar_same_goal(int x; int y){
 
     :: ((d != 1) && (d != 4)) -> printf("Avatar - D\\n");
 
+			if
+			:: d == 0 -> //Moved to an empty cell
+        map[x].a[y] = 0;
+				map[x+1].a[y] = 2;
+				x = x + 1
+			:: d == 3 -> //Moved to the goal, we won zulul
+				win = 1
+			fi
+
+		:: true -> c_code{printf("Avatar- Skip\\n");};//
+    fi;
+		lock = 0;
+  :: else ->	break
+  od;
+  //In game of thrones, you either win or die
+	if
+	:: (win == 1) -> printf("Avatar - Win\\n");
+	:: (dead == 1) -> printf("Avatar - Dead\\n");
+	fi;
+  lock = 0
+}
+"""
+promela_avatar_game_3_ltl_movement = """
+proctype avatar_same_goal(int x; int y){
+  map[x].a[y] = 2;
+  byte w, a, s, d;
+  bit foo;
+  //bool start = true;
+  do
+  ::((win == 0) && (dead == 0)) ->
+
+    lock;
+
+		if
+		:: dead == 1 -> break //Dude cannot win on opponent's turn anyways.
+    :: else -> skip
+		fi;
+    //Look-up:
+    w = map[x].a[y-1];
+    a = map[x-1].a[y];
+    s = map[x].a[y+1];
+    d = map[x+1].a[y];
+
+    if
+    :: ((w != 1) && (w != 4)) -> printf("Avatar - W\\n");
+      if
+      :: last_move == 0 -> straights++
+      :: else -> turns++
+      fi;
+      last_move = 0;
+      if
+      :: w == 0 -> //Moved to an empty cell
+        map[x].a[y] = 0;
+				map[x].a[y-1] = 2;
+				y = y - 1
+			:: w == 3 -> //Moved to the goal, we won zulul
+				win = 1
+			fi
+
+    :: ((a != 1) && (a != 4)) -> printf("Avatar - A\\n");
+      if
+      :: last_move == 1 -> straights++
+      :: else -> turns++
+      fi;
+      last_move = 1;
+			if
+			:: a == 0 -> //Moved to an empty cell
+        map[x].a[y] = 0;
+				map[x-1].a[y] = 2;
+				x = x - 1
+			:: a == 3 -> //Moved to the goal, we won zulul
+				win = 1
+			fi
+
+    :: ((s != 1) && (s != 4)) -> printf("Avatar - S\\n");
+      if
+      :: last_move == 2 -> straights++
+      :: else -> turns++
+      fi;
+      last_move = 2;
+			if
+			:: s == 0 -> //Moved to an empty cell
+        map[x].a[y] = 0;
+				map[x].a[y+1] = 2;
+				y = y + 1
+			:: s == 3 -> //Moved to the goal, we won zulul
+				win = 1
+			fi
+
+    :: ((d != 1) && (d != 4)) -> printf("Avatar - D\\n");
+      if
+      :: last_move == 3 -> straights++
+      :: else -> turns++
+      fi;
+      last_move = 3;
 			if
 			:: d == 0 -> //Moved to an empty cell
         map[x].a[y] = 0;
@@ -2137,6 +2249,11 @@ promela_ltl_formula_basic = """
 // LTL Formula : In any time, win never be true.
 // Counter-Example will be generated -> A scenario to win.
 ltl  { [] !win };
+"""
+promela_ltl_formula_movement = """
+// LTL Formula : In any time, win never be true.
+// Counter-Example will be generated -> A scenario to win.
+ltl  { []  (!win || (turns<10))};
 """
 promela_ltl_formula_2 = """
 // LTL Formula : In any time, win never be true.
@@ -4026,7 +4143,7 @@ class SpinClass_Game2_smart():
       raise spinCompileException("Cannot compile with gcc.")
     os.system("../spin/temp.out -a >/dev/null") #It's already nearly impossible to win this.
 
-class SpinClass_Game3_smart_LTL_Out():
+class SpinClass_Game3_smart():
   def __init__(self, map):
     self.map = map
     self.length = len(map)
@@ -4130,7 +4247,113 @@ class SpinClass_Game3_smart_LTL_Out():
       raise spinCompileException("Cannot compile with gcc.")
     os.system("../spin/temp.out -a -i >/dev/null")
 
-class SpinClass_Game3_smart():
+
+class SpinClass_Game3_smart_LTL_movement():
+  def __init__(self, map):
+    self.map = map
+    self.length = len(map)
+    self.width = len(map[0])#Assuming a rectangle
+    self.fixed_map = None
+    self.list_walls = []
+    self.promela_whole_file = """{}\n{}\n{}\n{}\n{}\n{}\n{}"""
+    self.wall_string = "{}"
+
+  def fix_map(self):
+    temp_map = []
+
+    for line in self.map:
+      temp_map.append(list(line))
+
+      checklist = 0
+
+    for lineNum, line in enumerate(temp_map):
+      for chNum, ch in enumerate(line):
+        if ch == '0':
+          temp_map[lineNum][chNum] = '.'
+        elif ch == '1':
+          temp_map[lineNum][chNum] = 'w'
+          self.list_walls.append((lineNum + 1, chNum + 1))
+        elif ch == 'A':
+          self.avatar_location = (lineNum, chNum)
+          checklist += 1
+        elif ch == 'G':
+          self.portal_location = (lineNum, chNum)
+          checklist += 1
+        elif ch == 'E':
+          self.enemy_location  = (lineNum, chNum)
+          checklist += 1
+
+    if checklist != 3:
+      raise spinCompileException("Inproper placement of sprites!")
+
+
+    self.fixed_map = []
+
+    to_attach = ""
+    to_attach_list = []
+    for i in range(0, self.width + 2):
+      to_attach_list.append('w')
+
+    to_attach = to_attach.join(to_attach_list)
+
+    for ln, line in enumerate(temp_map):
+      temp = ''.join(line)
+      temp = 'w' + temp + 'w'
+      self.fixed_map.append(temp)
+
+    self.fixed_map.insert(0, to_attach)
+    self.fixed_map.append(to_attach)
+
+  def create_wall_string(self):
+    for wall in self.list_walls:
+      self.wall_string = self.wall_string.format("\tmap[{}].a[{}] = 1;\n{}".format(wall[0], wall[1], "{}"))
+    self.wall_string = self.wall_string.format(" ")
+
+  def create_spin(self):
+    if self.fixed_map is None:
+      self.fix_map()
+    self.create_wall_string()
+
+    formatted_init = promela_init_for_game_3_smart.format(
+			avatar_y = self.avatar_location[0]+1,
+			avatar_x = self.avatar_location[1]+1,
+			portal_y = self.portal_location[0]+1,
+            portal_x = self.portal_location[1]+1,
+            opponent_y = self.enemy_location[0]+1,
+            opponent_x = self.enemy_location[1]+1,
+            length = self.length+1,
+            length2 = self.length,
+            width = self.width+1,
+            width2 = self.width,
+            wall_str = self.wall_string
+		)
+    formatted_header = promela_header_for_game_3_smart_ltl_movement.format(self.length+2, self.width+2)
+    self.promela_whole_file = self.promela_whole_file.format(
+			promela_comment_01,
+			promela_comment_02,
+			formatted_header,
+			promela_avatar_game_3_ltl_movement,
+			promela_opponent_for_game_3_smart,
+			formatted_init,
+			promela_ltl_formula_movement
+		)
+
+  def perform(self):
+    self.create_spin()
+    os.system("mkdir ../spin > /dev/null 2>&1")
+    os.system("rm ../spin/temp.pml > /dev/null")
+    f = open("../spin/temp.pml", "a")
+    f.write(self.promela_whole_file)
+    f.close()
+    os.system("spin -a ../spin/temp.pml")
+    proc = subprocess.Popen(["gcc -std=c99 pan.c -DREACH -DMAX_LEN={} -o ../spin/temp.out  -lm".format(self.length+2)], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    if out != b'':
+      raise spinCompileException("Cannot compile with gcc.")
+    os.system("../spin/temp.out -a -i >/dev/null")
+
+
+class SpinClass_Game3_smart_LTL_Out():
   def __init__(self, map):
     self.map = map
     self.length = len(map)
@@ -4459,7 +4682,7 @@ if __name__ == "__main__":
   cap = caPolisher.CApolisher(ca=ca)
   sp = spritePlanner.equalSpritePlanner(cap.perform())
   sp.perform()
-  ss = SpinClass_Game3_smart_LTL_Out(sp.getMap())
+  ss = SpinClass_Game3_smart_LTL_movement(sp.getMap())
   spp = spinParser.spinParser()
 
   ss.perform()
